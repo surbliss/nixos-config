e40a1595e80d286ad771cab9564e8861b61b94fe
System administrator <54353246+angryluck@users.noreply.github.com>
2024-10-18 11:30:51 +0200

    Changed to use shared flake

--- flake.nix
+++ flake.nix
@@ -4,29 +4,35 @@
 
   inputs = {
     # Replace "nixos-24.05" with system.stateVersion from configuration.nix
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    shared.url = "path:/etc/nix-config"; # Shared flake
+    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.follows = "shared/nixpkgs"; #
     # disko.url = "github:nix-community/disko";
     # disko.inputs.nixpkgs.follows = "nixpkgs";
     # home-manager.url = "github:nix-community/home-manager";
     # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = inputs@{ self, nixpkgs, ... }: {
+  outputs = {
+    self,
+    nixpkgs,
+    ...
+  }: {
     nixosConfigurations.angryluck = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       modules = [
-        ./configuration.nix 
+        ./configuration.nix
         # inputs.disko.nixosModules.disko
-	# {
-	#   home-manager.useGlobalPkgs = true;
-	#          home-manager.useUserPackages = true;
-	#          home-manager.users.angryluck = import ./home.nix;
-	#
-	#   # Optionally, use home-manager.extraSpecialArgs to pass
-	#          # arguments to home.nix
-	# }
+        # {
+        #   home-manager.useGlobalPkgs = true;
+        #          home-manager.useUserPackages = true;
+        #          home-manager.users.angryluck = import ./home.nix;
+        #
+        #   # Optionally, use home-manager.extraSpecialArgs to pass
+        #          # arguments to home.nix
+        # }
       ];
     };
   };
 }
-
 # vim: set sw=2
+
