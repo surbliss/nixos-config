f7de045c871b9a0ede8a1554c0ffa87b832b9d61
System administrator <54353246+angryluck@users.noreply.github.com>
2024-11-17 01:13:33 +0100

    Deleted shared flake, and updated flake

--- flake.nix
+++ flake.nix
@@ -4,35 +4,37 @@
 
   inputs = {
     # Replace "nixos-24.05" with system.stateVersion from configuration.nix
-    shared.url = "path:/etc/nix-config"; # Shared flake
-    # nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nixpkgs.follows = "shared/nixpkgs"; #
+    # shared.url = "path:/etc/nix-config"; # Shared flake
+    # nixpkgs.url
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    # nixpkgs.follows = "shared/nixpkgs";
     # disko.url = "github:nix-community/disko";
     # disko.inputs.nixpkgs.follows = "nixpkgs";
     # home-manager.url = "github:nix-community/home-manager";
     # home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: {
-    nixosConfigurations.angryluck = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
-      modules = [
-        ./configuration.nix
-        # inputs.disko.nixosModules.disko
-        # {
-        #   home-manager.useGlobalPkgs = true;
-        #          home-manager.useUserPackages = true;
-        #          home-manager.users.angryluck = import ./home.nix;
-        #
-        #   # Optionally, use home-manager.extraSpecialArgs to pass
-        #          # arguments to home.nix
-        # }
-      ];
+  outputs =
+    {
+      # self,
+      nixpkgs,
+      ...
+    }:
+    {
+      nixosConfigurations.angryluck = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [
+          ./configuration.nix
+          # inputs.disko.nixosModules.disko
+          # {
+          #   home-manager.useGlobalPkgs = true;
+          #          home-manager.useUserPackages = true;
+          #          home-manager.users.angryluck = import ./home.nix;
+          #
+          #   # Optionally, use home-manager.extraSpecialArgs to pass
+          #          # arguments to home.nix
+          # }
+        ];
+      };
     };
-  };
 }
 # vim: set sw=2
-
