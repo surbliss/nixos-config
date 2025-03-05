5a408eb2ed7d9b479b73f9e41b5f44e0e4b1b2c6
Thomas H. Surlykke <54353246+angryluck@users.noreply.github.com>
2025-03-05 18:42:50 +0100

    339 current  2025-03-05 18:42:46  25.05.20250303.ba487db  6.12.17                          *

--- shell-config.nix
+++ shell-config.nix
@@ -20,6 +20,7 @@
     #   # For jEdit to work!
     _JAVA_AWT_WM_NONREPARENTING = 1;
     DOTNET_CLI_TELEMETRY_OPTOUT = 1;
+    LC_MESSAGES = "en_US.UTF8";
     # Do this locally (if you must)
     # DOTNET_ROOT = "${pkgs.dotnet-sdk_7}/share/dotnet";
     #   AOCD_DIR = "$HOME/aocd/";
