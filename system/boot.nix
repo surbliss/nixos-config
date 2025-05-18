dab523e506a24557b35eb6913b2705997a423903
Thomas Holme Surlykke <54353246+angryluck@users.noreply.github.com>
2025-05-18 20:31:10 +0200

    Tried cleaning /tmp/ folder on boot

--- system/boot.nix
+++ system/boot.nix
@@ -1,18 +1,17 @@
 { ... }:
 {
-  boot = {
-    # systemd-boot EFI boot loader
-    loader.systemd-boot = {
-      enable = true;
-      # Disable editing kernel command-line before boot - otherwise, can get
-      # root access by passing init=bin/sh
-      editor = false;
-      # Set resultion of console
-      consoleMode = "auto";
-      # So /boot/ doesn't run out of memory
-      configurationLimit = 120;
-    };
-
-    loader.efi.canTouchEfiVariables = true;
+  # systemd-boot EFI boot loader
+  boot.loader.systemd-boot = {
+    enable = true;
+    # Disable editing kernel command-line before boot - otherwise, can get
+    # root access by passing init=bin/sh
+    editor = false;
+    # Set resultion of console
+    consoleMode = "auto";
+    # So /boot/ doesn't run out of memory
+    configurationLimit = 120;
   };
+
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.tmp.cleanOnBoot = true;
 }
