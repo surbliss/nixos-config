3427567af249c22fe30526bc27bd5d1b95c9ca9c
Thomas Holme Surlykke <54353246+angryluck@users.noreply.github.com>
2025-05-21 17:40:16 +0200

    Replacing tlp by auto-cpu-freq

--- system/power.nix
+++ system/power.nix
@@ -2,30 +2,56 @@
 {
   # Battery, see https://nixos.wiki/wiki/Laptop
   powerManagement.enable = true;
-  services = {
-    upower.enable = true; # Battery info
-    # thermald.enable = true; # Prevent overheating (primarily on intel CPUs)
-    tlp = {
-      enable = true;
-      settings = {
-        # Optional helps save long term battery health
-        # Change the AC-options if performance shit
-        CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-        CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-        CPU_ENERGY_PERF_POLICY_ON_AC = "power";
-        CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  services.upower.enable = true; # Battery info
 
-        CPU_MIN_PERF_ON_AC = 0;
-        CPU_MAX_PERF_ON_AC = 20;
-        CPU_MIN_PERF_ON_BAT = 0;
-        CPU_MAX_PERF_ON_BAT = 20;
+  # services.tlp = {
+  #   enable = true;
+  #   settings = {
+  #     # Optional helps save long term battery health
+  #     # Change the AC-options if performance shit
+  #     CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+  #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+  #     CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+  #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+  #
+  #     CPU_MIN_PERF_ON_AC = 0;
+  #     CPU_MAX_PERF_ON_AC = 20;
+  #     CPU_MIN_PERF_ON_BAT = 0;
+  #     CPU_MAX_PERF_ON_BAT = 20;
+  #
+  #     # Top one doesn't work for this laptop, but maybe next one
+  #     START_CHARGE_THRESH_BAT0 = 40; # % to start charging
+  #     STOP_CHARGE_THRESH_BAT0 = 80; # % to stop charging
+  #   };
+  # };
 
-        START_CHARGE_THRESH_BAT0 = 50; # 50 and bellow it starts to charge
-        STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+  # Also measures overheating
+  services.thermald.enable = true; # Prevent overheating (primarily on intel CPUs)
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      battery = {
+        # schedutil?
+        govenor = "powersave";
+        turbo = "never";
+        enable_thresholds = true;
+        start_threshhold = 40;
+        stop_threshold = 60;
       };
+      charger = {
+        govenor = "powersave";
+        turbo = "never";
+      };
+
     };
+
   };
 
   # Conflicts with tlp
-  services.power-profiles-daemon.enable = false;
+  # services.power-profiles-daemon.enable = false;
+
+  # See https://nixos.wiki/wiki/Power_Management
+  systemd.sleep.extraConfig = ''
+    AllowSuspendThenHibernate=no
+  '';
 }
