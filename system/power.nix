7aae6d61ec5ff74e2b0416950100476f1304d154
Thomas Holme Surlykke <54353246+angryluck@users.noreply.github.com>
2025-06-15 21:52:05 +0200

    Disable auto-cpufreq

--- system/power.nix
+++ system/power.nix
@@ -9,15 +9,15 @@
     settings = {
       # Optional helps save long term battery health
       # Change the AC-options if performance bad
-      # CPU_SCALING_GOVERNOR_ON_AC = "powersave";
-      # CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      # CPU_ENERGY_PERF_POLICY_ON_AC = "power";
-      # CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      #
-      # CPU_MIN_PERF_ON_AC = 0;
-      # CPU_MAX_PERF_ON_AC = 20;
-      # CPU_MIN_PERF_ON_BAT = 0;
-      # CPU_MAX_PERF_ON_BAT = 20;
+      CPU_SCALING_GOVERNOR_ON_AC = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "power";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+
+      CPU_MIN_PERF_ON_AC = 0;
+      CPU_MAX_PERF_ON_AC = 20;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
 
       # Top one doesn't work for this laptop, but maybe next one
       START_CHARGE_THRESH_BAT0 = 40; # % to start charging
@@ -25,7 +25,7 @@
     };
   };
 
-  services.auto-cpufreq.enable = true;
+  # services.auto-cpufreq.enable = true;
 
   # Also measures overheating
   # services.thermald.enable = true; # Prevent overheating (primarily on intel CPUs)
