28c46c231b4f5a09b2ad61376fb67ef67d1e7ca8
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-08-17 18:37:05 +0200

    Switch to niri
    Add statusbar
    
    Add xwayland satellite
    
    Also includes switching to wayland

---
+++ desktop/niri.nix
@@ -0,0 +1,44 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib)
+    mkEnableOption
+    mkIf
+    # mkOption
+    # mkPackageOption
+    # types
+    ;
+  cfg = config.custom.niri;
+in
+{
+  options.custom.niri = {
+    enable = mkEnableOption "niri window manager";
+  };
+
+  config = mkIf cfg.enable {
+    programs.niri.enable = true;
+
+    environment.systemPackages = with pkgs; [
+      mako
+      # xdg-desktop-portal-gtk
+      # xdg-desktop-portal-gnome
+      # gnome-keyring
+      # plasma-polkit-agent
+      waybar
+      alacritty
+      fuzzel
+      swaylock
+      xwayland-satellite
+      wl-clipboard
+      wlogout
+      wlr-randr
+      wayvnc
+      wf-recorder
+      wl-mirror
+    ];
+  };
+}
