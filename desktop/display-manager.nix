456239aa01ff10243e5210fe985e80db95489801
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-09-20 11:03:44 +0200

    Replace tuigreet by regreet

--- desktop/display-manager.nix
+++ desktop/display-manager.nix
@@ -1,21 +1,23 @@
 { pkgs, ... }:
 {
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = ''
-          ${pkgs.tuigreet}/bin/tuigreet \
-          --time \
-          --remember \
-          --cmd niri-session \
-          --asterisks \
-          --theme border=magenta;text=cyan;prompt=magenta;time=green;action=blue;button=yellow;container=black;input=white \
-        '';
-        user = "greeter";
-      };
-    };
-    useTextGreeter = true;
-  };
+  # services.greetd = {
+  #   enable = true;
+  #   settings = {
+  #     default_session = {
+  #       command = ''
+  #         ${pkgs.tuigreet}/bin/tuigreet \
+  #         --time \
+  #         --remember \
+  #         --cmd niri-session \
+  #         --asterisks \
+  #         --theme border=magenta;text=cyan;prompt=magenta;time=green;action=blue;button=yellow;container=black;input=white \
+  #       '';
+  #       user = "greeter";
+  #     };
+  #   };
+  #   useTextGreeter = true;
+  # };
+
+  programs.regreet.enable = true;
 
 }
