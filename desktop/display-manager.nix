df170bc0a2ebefc773614ef87bf62cbeffeb4893
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-09-20 10:25:03 +0200

    Styling tuigreet

--- desktop/display-manager.nix
+++ desktop/display-manager.nix
@@ -3,9 +3,15 @@
   services.greetd = {
     enable = true;
     settings = {
-      # terminal.vt = 2;
       default_session = {
-        command = "${pkgs.tuigreet}/bin/tuigreet --time --remember --cmd niri-session";
+        command = ''
+          ${pkgs.tuigreet}/bin/tuigreet \
+          --time \
+          --remember \
+          --cmd niri-session \
+          --asterisks \
+          --theme border=magenta;text=cyan;prompt=magenta;time=green;action=blue;button=yellow;container=black;input=white \
+        '';
         user = "greeter";
       };
     };
