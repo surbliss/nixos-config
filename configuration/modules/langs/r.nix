050702d512da8f1d25ee00ef1b40ed0c5dda1a08
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2025-12-07 19:46:04 +0100

    Move Rstudio to system install

--- configuration/modules/langs/r.nix
+++ configuration/modules/langs/r.nix
@@ -10,23 +10,45 @@
         ];
       };
       qt.enable = true;
+      environment.systemPackages =
+        let
+          inherit (pkgs) rPackages rstudioWrapper;
+          packages = with rPackages; [
+            tinytex
+            rmarkdown
+            # Other stuff for R Markdown
+            magrittr
+            stringi
+            stringr
+          ];
+          rstudio-with-packages = rstudioWrapper.override { inherit packages; };
+        in
+        with pkgs;
+        [
+          rstudio-with-packages
+          # Suggested dependencies from eclaude
+          glib
+          gtk3
+          libglvnd
+
+        ];
     };
 
-  flake.modules.homeManager.gui =
-    { pkgs, ... }:
-    let
-      inherit (pkgs) rPackages rstudioWrapper;
-      packages = with rPackages; [
-        tinytex
-        rmarkdown
-        # Other stuff for R Markdown
-        magrittr
-        stringi
-        stringr
-      ];
-      rstudio-with-packages = rstudioWrapper.override { inherit packages; };
-    in
-    {
-      home.packages = [ rstudio-with-packages ];
-    };
+  # flake.modules.homeManager.gui =
+  #   { pkgs, ... }:
+  #   let
+  #     inherit (pkgs) rPackages rstudioWrapper;
+  #     packages = with rPackages; [
+  #       tinytex
+  #       rmarkdown
+  #       # Other stuff for R Markdown
+  #       magrittr
+  #       stringi
+  #       stringr
+  #     ];
+  #     rstudio-with-packages = rstudioWrapper.override { inherit packages; };
+  #   in
+  #   {
+  #     home.packages = [ rstudio-with-packages ];
+  #   };
 }
