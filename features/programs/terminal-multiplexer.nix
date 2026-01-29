20c644341e6b2fd1d069f9ebe825e28803752430
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-01-29 15:06:02 +0100

    Refresh direnv when resurrecting zellij

--- features/programs/terminal-multiplexer.nix
+++ features/programs/terminal-multiplexer.nix
@@ -1,8 +1,18 @@
 {
+
   flake.modules.homeManager.cli =
     { pkgs, custom-link, ... }:
+    let
+      with-nix = pkgs.writeShellScriptBin "with-nix" ''
+        eval "$(direnv export zsh)"
+        exec "$@"
+      '';
+    in
     {
-      home.packages = with pkgs; [ zellij ];
+      home.packages = with pkgs; [
+        zellij
+        with-nix
+      ];
       xdg.configFile = custom-link "zellij";
 
     };
