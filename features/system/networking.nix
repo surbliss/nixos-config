6fc9f1a3fb89c4ddcaf31557f180cc30eb1b6936
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-02-03 12:34:49 +0100

    iwd from wpa_supplicant

--- features/system/networking.nix
+++ features/system/networking.nix
@@ -4,9 +4,19 @@
     {
 
       # Don't enable iwd, doesn't mash well with eduroam
-      networking.networkmanager.enable = true;
-      networking.networkmanager.wifi.powersave = false;
+      networking.networkmanager = {
+        enable = true;
+        wifi.backend = "iwd";
+        wifi.powersave = false;
+      };
+      networking.wireless.iwd.enable = true;
+      networking.wireless.iwd.settings = {
+        General.EnableNetworkConfiguration = true;
+        Network.EnableIPv6 = true;
+        Settings.AutoConnect = true;
+      };
 
+      services.gnome.gnome-keyring.enable = true;
       # Remember to set host-name in host-configuration
       # Also add "networkmanager" to users 'extraGroups'
 
