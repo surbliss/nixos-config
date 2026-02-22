dc0ecbaf883dd2e48bc07afc300ea0b880d0732f
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-02-22 09:32:40 +0100

    Enable flake-parts debug

---
+++ features/debug.nix
@@ -0,0 +1,5 @@
+# Allows repl introspection into 'allSystem' and 'currentSystem' after
+# > nix repl
+# > :lf .
+# See https://flake.parts/options/flake-parts.html?highlight=debug#opt-debug
+{ debug = true; }
