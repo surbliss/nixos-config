bcd4756ae69d3fe4fcf6f92b8949ebdfb4bcb723
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-03-07 16:13:53 +0100

    Add DankMaterialShell + display-manager to mangowc + base-line default config
    Base-line default config for dms

--- features/desktop/mangowc.nix
+++ features/desktop/mangowc.nix
@@ -4,12 +4,38 @@
   flake.modules.nixos.desktop = moduleWithSystem (
     { inputs', ... }:
     { pkgs, ... }:
+    let
+      mango = inputs'.mangowc.packages.mango;
+    in
     {
+      ### Import mango-module from flake
       imports = [ inputs.mangowc.nixosModules.mango ];
+
+      ### DankMaterialShell
+      programs.dms-shell.enable = true;
+
+      ### DankMateralGreeter
+      programs.sway.enable = true; # For running the greeter in
+      services.displayManager.dms-greeter = {
+        enable = true;
+        compositor = {
+          name = "sway"; # Required. Can be also "hyprland" or "niri"
+          # Disable mouse acceleration
+          customConfig = ''
+            input type:pointer {
+              accel_profile flat
+            }
+          '';
+        };
+
+        # Sync your user's DankMaterialShell theme with the greeter. You'll probably want this
+        configHome = "/home/angryluck";
+
+      };
+
       programs.mango.enable = true;
       # Add the default config
-      environment.etc."mango".source =
-        "${inputs'.mangowc.packages.mango}/etc/mango";
+      environment.etc."mango".source = "${mango}/etc/mango";
       xdg.portal = {
         enable = true;
         wlr.enable = true;
@@ -18,18 +44,19 @@
     }
   );
   flake.modules.homeManager.desktop =
-    { pkgs, ... }:
+    { pkgs, custom-link, ... }:
     {
+      xdg.configFile = custom-link "mango";
       home.packages = with pkgs; [
         ### Packages that default config uses
         foot
-        rofi
+        # rofi
 
         ### Suggested packages, see https://mangowc.vercel.app/docs/quick-start/
-        fuzzel
+        # fuzzel
         wezterm
-        waybar
-        noctalia-shell
+        # waybar
+        # noctalia-shell
         swaybg
         wl-clipboard
         wl-clip-persist
@@ -39,9 +66,9 @@
 
         wlogout
 
-        dunst
+        # dunst
 
-        polkit_gnome
+        # polkit_gnome
       ];
 
     };
