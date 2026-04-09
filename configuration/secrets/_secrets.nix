95f97cee90cf70e85b1b32d4636e5ad67270ff09
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-04-09 14:28:23 +0200

    Add surface-book public ssh key to agenix secrets

--- configuration/secrets/_secrets.nix
+++ configuration/secrets/_secrets.nix
@@ -1,14 +1,15 @@
 # Important - _not_ to be imported into NixOS-config
 let
-
   asus21-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG/LpSdViH0+Ksk93UINInLOX5UANVCLWOAUHYMpT00K";
   asus21-user = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAID2I6rQN0INm8Y4lajgTzgTZdBX1U/9NdiqtZ3xYjwoj";
-  asus21 = [
+  surface-book-system = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHQSzkjLsfRzeDRpgZQI+KXfdqHq3vqt2MnInr4AH3qG root@surface-book";
+  all-systems = [
     asus21-system
     asus21-user
+    surface-book-system
   ];
 in
 {
-  "KU_PASSWORD.age".publicKeys = asus21;
-  "KU_MAIL.age".publicKeys = asus21;
+  "KU_PASSWORD.age".publicKeys = all-systems;
+  "KU_MAIL.age".publicKeys = all-systems;
 }
