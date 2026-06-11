1a4a75342ede6a7c250eec6b63e96812c001ba62
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-06-11 15:51:53 +0200

    Install wl-clipboard

--- features/desktop/clipboard.nix
+++ features/desktop/clipboard.nix
@@ -1,7 +1,8 @@
 {
   # Unsure if 'desktop' is the right module  here :/
-  flake.modules.homeManager.desktop = {
+  flake.modules.homeManager.desktop = { pkgs, ... }: {
     services.wl-clip-persist.enable = true;
     services.cliphist.enable = true;
+    home.packages = with pkgs; [ wl-clipboard ];
   };
 }
