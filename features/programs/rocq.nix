1da3d91a3464e301be8741a0353362c56f8b3e24
Thomas Holme Surlykke <thomas.dev@surlykke.dk>
2026-07-29 16:49:07 +0200

    Fix rocq (coq) install to include rocqide
    Add RocqIDE desktop entry

--- features/programs/rocq.nix
+++ features/programs/rocq.nix
@@ -1,13 +1,30 @@
 {
-  flake.modules.homeManager.gui = { pkgs, ... }: {
-    home.packages = with pkgs; [
-      # rocq-core
-      (coq.withPackages (
+  flake.modules.nixos.gui =
+    { pkgs, lib, ... }:
+    let
+      # coq-with-ide = pkgs.coq.override { buildIde = true; };
+      coq-with-ide = pkgs.coq.override { buildIde = true; };
+      coq-with-packages = coq-with-ide.withPackages (
         p: with p; [
           ssprove
           vscoq-language-server
         ]
-      ))
-    ];
+      );
+    in
+    {
+      environment.systemPackages = [
+        (lib.lowPrio coq-with-ide)
+        coq-with-packages
+      ];
+
+    };
+
+  flake.modules.homeManager.gui = {
+    xdg.desktopEntries.rocqide = {
+      type = "Application";
+      exec = "env GTK_THEME=Adwaita:light rocqide";
+      name = "RocqIDE";
+      terminal = false;
+    };
   };
 }
